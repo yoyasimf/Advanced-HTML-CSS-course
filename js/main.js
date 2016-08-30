4d3af1bfd4e72e16e4823b8bc8841d562bb6edee
yoyasimf <voffka93@gmail.com>
2016-08-30 18:00:34 +0300

    add adaptive 2.0

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 // Can also be used with $(document).ready()
 $(window).load(function() {
+  // $('.nav-toggle').on('click', function(){
+  //  $(this).toggleClass('close-nav');
+  //  $('.navigation').toggleClass('open');
+  //});
+  //$('.navigation li a').on('click', function(){
+  //  $('.navigation').removeClass("open");
+  //});
+
   $('.flexslider').flexslider({
     animation: "fade",
     directionNav: false
   });
+
 });
